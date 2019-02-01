26c9a8d4cfd5e53dd5ed56b573a170ffec02941e
Rafael Hernández Larios <rafah.larios@gmail.com>
2019-02-01 15:17:31 -0600

    :bug: fix script value package.json

--- appendScript.js
+++ appendScript.js
@@ -7,5 +7,5 @@ const relativePath = env.npm_package_name;
 const pathFile = `${env.INIT_CWD}/package.json`;
 
 const content = require(pathFile);
-content.scripts.test = `sh node_modules/${relativePath}/deploy.sh $env`;
+content.scripts.deploy = `sh node_modules/${relativePath}/deploy.sh $env`;
 fs.writeFileSync(pathFile, JSON.stringify(content));
