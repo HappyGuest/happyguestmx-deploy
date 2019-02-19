009093f7971fa266fd1ef2ee627e28ead74304c0
Rafael Hernández Larios <rafah.larios@gmail.com>
2019-02-18 23:46:59 -0600

    :bug: add package.json if it doesnt exist

--- appendScript.js
+++ appendScript.js
@@ -3,9 +3,38 @@
 const fs = require('fs'),
     env = process.env;
 
-const relativePath = env.npm_package_name;
-const pathFile = `${env.INIT_CWD}/package.json`;
+const relativePath = env.npm_package_name,
+    pathFile = `${env.INIT_CWD}/package.json`;
 
-const content = require(pathFile);
-content.scripts.deploy = `sh node_modules/${relativePath}/deploy.sh $env`;
-fs.writeFileSync(pathFile, JSON.stringify(content));
+let packageFile;
+
+function appendScript() {
+    try {
+        packageFile = require(pathFile);
+        writePackage(packageFile)
+    }
+    catch (err) {
+        if (err.code === 'MODULE_NOT_FOUND') {
+            packageFile = require('./src/package.json');
+            writePackage(packageFile);
+        } else console.log(err);
+    }
+}
+
+function writePackage(packageFile) {
+    try {
+        const scriptCommand = `sh node_modules/${relativePath}/deploy.sh $env`;
+        if (packageFile.scripts) {
+            packageFile.scripts.deploy = scriptCommand;
+        }
+        else {
+            packageFile.scripts = { deploy: scriptCommand };
+        }
+        fs.writeFileSync(pathFile, JSON.stringify(packageFile, null, 4));
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+appendScript();
